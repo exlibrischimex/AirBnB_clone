647b6fcdbd17b42e8e981b091978d9602172f34c
exlibrischimex <exlibrischimex@gmail.com>
2022-08-10 03:24:30 +0100

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" Class Amenity """
+"""
+Defines amenities
+"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Amenity class that inherits BaseModel"""
-    name = "
+    """Defines amenities that user can choose from to offer at its place"""
+    name = ""
